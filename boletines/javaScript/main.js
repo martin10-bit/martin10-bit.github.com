2041f99631fba8520933fdd497e1508181e63ccc
martin10-bit <69392870+martin10-bit@users.noreply.github.com>
2021-06-08 00:02:03 +0200

    redirecciona a /boletines

--- boletines/javaScript/main.js
+++ boletines/javaScript/main.js
@@ -55,7 +55,7 @@ links.forEach(function(link){
         content.classList.add("animate__animated");
 
         setTimeout(function(){
-        location.href = "/";
+        location.href = "/boletines";
         },600);
 
         return false;
