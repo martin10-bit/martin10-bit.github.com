70d3d08620ff8a24c4b0f86a91763361512316bd
martin10-bit <69392870+martin10-bit@users.noreply.github.com>
2021-06-07 23:54:02 +0200

    Agregamos el proyecto boletines

---
+++ boletines/javaScript/main.js
@@ -0,0 +1,66 @@
+// console.log("hola desde el archivo javascript");
+
+
+
+
+
+// DOM 
+// querySelector  // querySelectorAll
+/*
+let links = document.querySelectorAll("a");
+
+links.forEach(function(link){
+    console.log(link);
+});
+*/
+
+
+
+
+
+// ejemplo de accion CLICK
+
+/*
+let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+    td.addEventListener('click', function(){
+        console.log("click");
+    })
+});
+*/
+
+
+
+
+
+
+// Obtener los elementos de la clase .close
+let links = document.querySelectorAll(".close");
+
+// Recorrerlos
+links.forEach(function(link){
+
+    // Agregar un evento click a cada uno de ellos ev=evento
+    link.addEventListener("click", function(ev){
+        ev.preventDefault();
+        let content = document.querySelector(".content");
+
+        // Quitarle las clases de animacion que ya tiene
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+
+        // Agregar clases para animar su salida fadeOutUp
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+        setTimeout(function(){
+        location.href = "/";
+        },600);
+
+        return false;
+
+    });
+
+});
+
